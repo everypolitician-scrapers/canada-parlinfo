0a105d8137daf1a73f28cb4875f227581ffcc92a
Tony Bowden <tony@tmtm.com>
2017-06-28 07:29:03 +0100

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'colorize'
 require 'combine_popolo_memberships'
@@ -14,7 +15,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -25,17 +26,15 @@ end
 def scrape_list(url)
   noko = noko_for(url)
   noko.xpath('//table[@id="ctl00_cphContent_ctl00_grdMembersList"]//tr[td]//td[1]/a').each do |a|
-    data = { 
-      id: CGI.parse(URI.parse(a.attr('href')).query)['Item'].first,
+    data = {
+      id:   CGI.parse(URI.parse(a.attr('href')).query)['Item'].first,
       name: a.text,
     }
     ScraperWiki.save_sqlite([:id], data, 'sources')
   end
 end
 
-
 class Person
-
   PERSON_URL = 'http://www.lop.parl.gc.ca/parlinfo/Files/Parliamentarian.aspx?Item=%s&Language=E'
 
   def initialize(h)
@@ -43,7 +42,7 @@ class Person
   end
 
   def member_data
-    @md ||= Hash[ protected_methods.map { |m| [m, send(m)] } ]
+    @md ||= Hash[protected_methods.map { |m| [m, send(m)] }]
   end
 
   def combined_memberships
@@ -62,11 +61,11 @@ class Person
   end
 
   def name
-    [given_name, family_name].join " "
+    [given_name, family_name].join ' '
   end
 
   def sort_name
-    [family_name, given_name].join ", "
+    [family_name, given_name].join ', '
   end
 
   def family_name
@@ -78,12 +77,12 @@ class Person
   end
 
   def birth_date
-    date_str = noko.css('#ctl00_cphContent_DateOfBirthData').text.gsub('.','-')
+    date_str = noko.css('#ctl00_cphContent_DateOfBirthData').text.tr('.', '-')
     return date_str.empty? ? nil : Date.parse(date_str).to_s rescue nil
   end
 
   def death_date
-    date_str = noko.css('#ctl00_cphContent_DeceasedDateData').text.gsub('.','-') 
+    date_str = noko.css('#ctl00_cphContent_DeceasedDateData').text.tr('.', '-')
     return date_str.empty? ? nil : Date.parse(date_str).to_s rescue nil
   end
 
@@ -118,18 +117,18 @@ class Person
     noko.css('#ctl00_cphContent_ctl00_pnlSectionHouseOfCommons')
   end
 
- def elected
+  def elected
     commons.xpath('.//table[@id="ctl00_cphContent_ctl00_grdHouseOfCommons"]//tr[td]').map do |tr|
       td = tr.css('td')
       next if td.last.text == 'Defeated'
       raise "Unknown result: #{td.last.text}" unless td.last.text == 'Elected'
-      date = Date.parse(td[2].css('span').find { |s| s.attr('id').include? 'Label' }.text.tidy.gsub('.','-')).to_s
+      date = Date.parse(td[2].css('span').find { |s| s.attr('id').include? 'Label' }.text.tidy.tr('.', '-')).to_s
       term = term_for(date) or next
       {
-        id: term[:id],
+        id:           term[:id],
         constituency: td[0].css('span').find { |s| s.attr('id').include? 'Label' }.text.tidy,
-        start_date: date,
-        end_date: term[:end_date], # no record of people leaving early?
+        start_date:   date,
+        end_date:     term[:end_date], # no record of people leaving early?
       }
     end.compact
   end
@@ -137,19 +136,19 @@ class Person
   def group_memberships
     noko.xpath('.//table[@id="ctl00_cphContent_ctl00_grdCaucus"]//tr[td]').map do |tr|
       td = tr.css('td')
-      start_date, end_date = td.last.text.tidy.split(' - ').map { |d| d.gsub('.','-') }
+      start_date, end_date = td.last.text.tidy.split(' - ').map { |d| d.tr('.', '-') }
       {
-        id: td.first.text.tidy,
+        id:         td.first.text.tidy,
         start_date: start_date,
-        end_date: end_date,
+        end_date:   end_date,
       }
     end
   end
 
   def minister
-    #TODO bd9df755-dfce-42dc-9ebb-55306d610edc
+    # TODO: bd9df755-dfce-42dc-9ebb-55306d610edc
   end
-    
+
   TERM_SOURCE = 'https://raw.githubusercontent.com/everypolitician/everypolitician-data/master/data/Canada/Commons/sources/manual/terms.csv'
   def all_terms
     @_terms = CSV.parse(open(TERM_SOURCE).read, headers: true, header_converters: :symbol).sort_by { |t| t[:start_date] }.reverse
@@ -162,11 +161,10 @@ class Person
   end
 end
 
-
 def scrape_person(id)
   p = Person.new(id: id)
   data = p.data or return
-  ScraperWiki.save_sqlite([:id, :term, :start_date], data) 
+  ScraperWiki.save_sqlite(%i[id term start_date], data)
 end
 
 toget = ScraperWiki.select('DISTINCT(id) FROM sources').map { |i| i['id'] } rescue nil
@@ -175,12 +173,11 @@ if toget
   got = ScraperWiki.select('DISTINCT(id) FROM data').map { |i| i['id'] } rescue nil
   wanted = got ? toget - got : toget
   warn "#{wanted.count} to fetch"
-  wanted.each_with_index do |r, i| 
+  wanted.each_with_index do |r, i|
     warn i if (i % 10).zero?
     scrape_person r
   end
 else
   scrape_list('http://www.lop.parl.gc.ca/parlinfo/Lists/Members.aspx?New=False&Current=False')
-  warn "List updated"
+  warn 'List updated'
 end
-
